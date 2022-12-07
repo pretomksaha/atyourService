9caa9af41d193c835e09490848951b1f9badea9c
pretomksaha <pretomksaha@yahoo.com>
2022-12-07 11:47:31 +0100

    Initialize backend system with Spring Boot

---
+++ atService/src/main/java/com/example/atService/AtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.atService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtServiceApplication.class, args);
+	}
+
+}
